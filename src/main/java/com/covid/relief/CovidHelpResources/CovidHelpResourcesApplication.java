6279bfe4ac909cfd97b3e561248b6d306b6583d5
neeraj7 <kneeraj508@gmail.com>
2021-04-29 21:21:24 +0530

    Initial commit.

---
+++ src/main/java/com/covid/relief/CovidHelpResources/CovidHelpResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.covid.relief.CovidHelpResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidHelpResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidHelpResourcesApplication.class, args);
+	}
+
+}
